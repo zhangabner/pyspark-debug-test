128b3ab7ae36e7a46ecb661e63e3e2bc764d694c
Abner Zhang <abner.zhangxinbin.anz@gmail.com>
2021-11-16 08:45:57 +1100

    debug

--- debugtest.py
+++ debugtest.py
@@ -1,8 +1,21 @@
-content = "How happy I am that I am gone"
-rdd = sc.parallelize(content.split(' '))\
-    .map(lambda x: (x, 1))\
-    .reduceByKey(lambda x,y: x + y)
+from pyspark import SparkFiles
+from pyspark.sql import Row, SparkSession, DataFrame
 
-rdd.toDF(['word','count'])\
-    .orderBy('count')\
-    .show()
+test_data = [
+    {
+        "ID": 1,
+        "First_Name": "Bob",
+        "Last_Name": "Builder",
+        "Age": 24
+    },
+    {
+        "ID": 2,
+        "First_Name": "Sam",
+        "Last_Name": "Smith",
+        "Age": 41
+    }
+]
+
+spark = SparkSession.builder.getOrCreate()
+test_df = spark.createDataFrame(map(lambda x: Row(**x), test_data))
+test_df.show(1)
---
+++ tests/dev_etl_job.py
@@ -0,0 +1,205 @@
+
+class Log4j(object):
+    """Wrapper class for Log4j JVM object.
+
+    :param spark: SparkSession object.
+    """
+
+    def __init__(self, spark):
+        # get spark app details with which to prefix all messages
+        conf = spark.sparkContext.getConf()
+        app_id = conf.get('spark.app.id')
+        app_name = conf.get('spark.app.name')
+
+        log4j = spark._jvm.org.apache.log4j
+        message_prefix = '<' + app_name + ' ' + app_id + '>'
+        self.logger = log4j.LogManager.getLogger(message_prefix)
+
+    def error(self, message):
+        """Log an error.
+
+        :param: Error message to write to log
+        :return: None
+        """
+        self.logger.error(message)
+        return None
+
+    def warn(self, message):
+        """Log a warning.
+
+        :param: Warning message to write to log
+        :return: None
+        """
+        self.logger.warn(message)
+        return None
+
+    def info(self, message):
+        """Log information.
+
+        :param: Information message to write to log
+        :return: None
+        """
+        self.logger.info(message)
+        return None
+"""
+test_etl_job.py
+~~~~~~~~~~~~~~~
+
+This module contains unit tests for the transformation steps of the ETL
+job defined in etl_job.py. It makes use of a local version of PySpark
+that is bundled with the PySpark package.
+"""
+import unittest
+
+import json
+
+from pyspark.sql.functions import mean
+
+# from dependencies.spark import start_spark
+from etl_job import transform_data
+
+from os import environ, listdir, path
+import json
+from pyspark import SparkFiles
+from pyspark.sql import SparkSession
+
+# from dependencies import logging
+
+
+def start_spark(app_name='my_spark_app', master='local[*]', jar_packages=[],
+                files=[], spark_config={}):
+    """Start Spark session, get Spark logger and load config files.
+
+    Start a Spark session on the worker node and register the Spark
+    application with the cluster. Note, that only the app_name argument
+    will apply when this is called from a script sent to spark-submit.
+    All other arguments exist solely for testing the script from within
+    an interactive Python console.
+
+    This function also looks for a file ending in 'config.json' that
+    can be sent with the Spark job. If it is found, it is opened,
+    the contents parsed (assuming it contains valid JSON for the ETL job
+    configuration) into a dict of ETL job configuration parameters,
+    which are returned as the last element in the tuple returned by
+    this function. If the file cannot be found then the return tuple
+    only contains the Spark session and Spark logger objects and None
+    for config.
+
+    The function checks the enclosing environment to see if it is being
+    run from inside an interactive console session or from an
+    environment which has a `DEBUG` environment variable set (e.g.
+    setting `DEBUG=1` as an environment variable as part of a debug
+    configuration within an IDE such as Visual Studio Code or PyCharm.
+    In this scenario, the function uses all available function arguments
+    to start a PySpark driver from the local PySpark package as opposed
+    to using the spark-submit and Spark cluster defaults. This will also
+    use local module imports, as opposed to those in the zip archive
+    sent to spark via the --py-files flag in spark-submit.
+
+    :param app_name: Name of Spark app.
+    :param master: Cluster connection details (defaults to local[*]).
+    :param jar_packages: List of Spark JAR package names.
+    :param files: List of files to send to Spark cluster (master and
+        workers).
+    :param spark_config: Dictionary of config key-value pairs.
+    :return: A tuple of references to the Spark session, logger and
+        config dict (only if available).
+    """
+
+    # detect execution environment
+    # flag_repl = not(hasattr(__main__, '__file__'))
+    flag_debug = 'DEBUG' in environ.keys()
+
+    # if not (flag_repl or flag_debug):
+    #     # get Spark session factory
+    #     spark_builder = (
+    #         SparkSession
+    #         .builder
+    #         .appName(app_name))
+    # else:
+    if True:
+        # get Spark session factory
+        spark_builder = (
+            SparkSession
+            .builder
+            .master(master)
+            .appName(app_name))
+
+        # create Spark JAR packages string
+        spark_jars_packages = ','.join(list(jar_packages))
+        spark_builder.config('spark.jars.packages', spark_jars_packages)
+
+        spark_files = ','.join(list(files))
+        spark_builder.config('spark.files', spark_files)
+
+        # add other config params
+        for key, val in spark_config.items():
+            spark_builder.config(key, val)
+
+    # create session and retrieve Spark logger object
+    spark_sess = spark_builder.getOrCreate()
+    spark_logger = Log4j(spark_sess)
+
+    # get config file if sent to cluster with --files
+    spark_files_dir = SparkFiles.getRootDirectory()
+    config_files = [filename
+                    for filename in listdir(spark_files_dir)
+                    if filename.endswith('config.json')]
+
+    if config_files:
+        path_to_config_file = path.join(spark_files_dir, config_files[0])
+        with open(path_to_config_file, 'r') as config_file:
+            config_dict = json.load(config_file)
+        spark_logger.warn('loaded config from ' + config_files[0])
+    else:
+        spark_logger.warn('no config file found')
+        config_dict = None
+
+    return spark_sess, spark_logger, config_dict
+
+
+config = json.loads("""{"steps_per_floor": 21}""")
+spark, *_ = start_spark()
+test_data_path = '/home/u18/pyspark-debug-test/tests/test_data/'
+
+
+# assemble
+input_data = (
+    spark
+    .read
+    .parquet(test_data_path + 'employees'))
+
+expected_data = (
+    spark
+    .read
+    .parquet(test_data_path + 'employees_report'))
+
+expected_cols = len(expected_data.columns)
+expected_rows = expected_data.count()
+expected_avg_steps = (
+    expected_data
+    .agg(mean('steps_to_desk').alias('avg_steps_to_desk'))
+    .collect()[0]
+    ['avg_steps_to_desk'])
+# act
+data_transformed = transform_data(input_data, 21)
+
+input_data.show(5)
+data_transformed.show(5)
+
+
+cols = len(expected_data.columns)
+rows = expected_data.count()
+avg_steps = (
+    expected_data
+    .agg(mean('steps_to_desk').alias('avg_steps_to_desk'))
+    .collect()[0]
+    ['avg_steps_to_desk'])
+
+# assert
+# assertEqual(expected_cols, cols)
+# assertEqual(expected_rows, rows)
+# assertEqual(expected_avg_steps, avg_steps)
+# assertTrue([col in expected_data.columns
+#                     for col in data_transformed.columns])
+
